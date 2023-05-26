b5141b60d037414d71569a3f399e6304c67a42b8
Leonardo Alves <leonardo.rba@uol.com.br>
2023-05-26 15:34:15 -0300

    rails watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: %i[index show new create destroy] do
+    resources :bookmarks, only: %i[new create destroy]
+  end
 end
